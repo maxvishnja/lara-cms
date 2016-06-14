9c018e1084d43309d3b72b23164e720e8ef957b9
Администратор <adm@Mac-Pro-Aleksandr.local>
2016-06-14 15:28:40 +0300

    add package Sentinel for users roles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,9 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+
+
+                <div class="title">Hello {{ Sentry::getUser()->username }}</div>
             </div>
         </div>
     </body>
