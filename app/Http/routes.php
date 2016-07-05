8216c0da37903045a76f64dfc94a42ee60cb4aa3
Ivan Kolesnikov <impalago@yandex.ru>
2016-07-05 13:50:15 +0300

    change route customers

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,12 +16,7 @@ Route::group(['middleware' => ['sentry.auth']], function () {
         return View::make('modules/main.index');
     }));
 
-    Route::get('customers', ['as' => 'customers.index', 'uses' => 'CustomersController@index']);
-    Route::get('customers/create', ['as' => 'customers.create', 'uses' => 'CustomersController@create']);
-    Route::post('customers', ['as' => 'customers.store', 'uses' => 'CustomersController@store']);
-    Route::get('customers/{id}', ['as' => 'customers.show', 'uses' => 'CustomersController@show']);
-    Route::get('customers/{id}/edit', ['as' => 'customers.edit', 'uses' => 'CustomersController@edit']);
-    Route::put('customers/{id}', ['as' => 'customers.update', 'uses' => 'CustomersController@update']);
-    Route::delete('customers/{id}', ['as' => 'customers.destroy', 'uses' => 'CustomersController@destroy']);
+    // Customers
+    Route::resource('customers', 'CustomersController');
 });
 
